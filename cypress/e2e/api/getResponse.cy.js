1799e9b73380f16e283c36516a2ca9ac95739fc1
shreyas2001 <67593308+shreyas2001@users.noreply.github.com>
2025-11-14 21:34:47 +0530

    Cypress framework commit finals run : add

--- cypress/e2e/api/getResponse.cy.js
+++ cypress/e2e/api/getResponse.cy.js
@@ -1,12 +1,13 @@
 import AxiosClient from '../../utils/axiosApiCalls';
+import Logger from '../../utils/logUtility';
 
 describe("Checking GET api response",() =>{
     it("Get api response",() =>{
         Logger.info('Sending GET request...');
         return AxiosClient.get("https://jsonplaceholder.typicode.com/todos/1").then((response) =>{
             expect(response.status).to.eq(200);
-            expect(response.body).to.have.property('id');
-            expect(response.body).to.have.property('userId');
+            expect(response.data).to.have.property('id');
+            expect(response.data).to.have.property('userId');
         })
     })
 })
