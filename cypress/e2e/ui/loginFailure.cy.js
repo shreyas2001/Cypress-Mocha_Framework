55615f739aea1fb667542ad72c22f89a863e1fc5
shreyas2001 <67593308+shreyas2001@users.noreply.github.com>
2025-11-16 11:53:30 +0530

    pushing only one file for final run: update artiacts and schduled runs

--- cypress/e2e/ui/loginFailure.cy.js
+++ cypress/e2e/ui/loginFailure.cy.js
@@ -1,6 +1,7 @@
-// const LoginPage = require('../../pages/loginPage');
 import loginData from '../../fixtures/loginData.json';
 import LoginPage from '../../pages/loginPage';
+import './commands';
+
 
 describe('Login Failure Test', () => {
     it('Should show error message for invalid login', () => {
